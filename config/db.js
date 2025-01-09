0f6d8d39358a347dd8720c1b9736d8971be7b663
Ismail Bin Mujeeb <IsmailBinMujeeb@gmail.com>
2025-01-09 12:51:26 +0530

    mongoDB setup

---
+++ config/db.js
@@ -0,0 +1,5 @@
+const mongoose = require('mongoose');
+
+const conn = mongoose.connect(process.env.MDB_CONN_STR).then(console.log('Successfuly connected to MongoDB')).catch(e=>console.log(e));
+
+module.exports = conn;
